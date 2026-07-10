e11b2cd6fd6a007329a10e6d2891b49dc2cb1270
tgervasius-lgtm <tgervasius@gmail.com>
2026-07-10 09:11:02 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,12 @@
-const CACHE_NAME='bss-interactive-demo-v2';
-const ASSETS=['./','./index.html','./manifest.json','./icons/icon.svg'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request)))})
+const CACHE_NAME = 'bss-polished-green-v1';
+const ASSETS = ['./','./index.html','./manifest.json','./icons/icon.svg'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request)));
+});
